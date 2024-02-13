eeafc64955d660584ac7e19cc998972a0d0d5fba
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-02-13 14:51:17 +0600

    new pentagon section added

--- scripts/pentagon.js
+++ scripts/pentagon.js
@@ -1,1 +1,20 @@
-console.log(connected)
+function pentagonArea(){
+    const perimeter = getInputValueById('pentagon-perimeter');
+    // console.log(perimeter);
+    const apothem = getInputValueById('pentagon-apothem');
+    const area = 0.5 * perimeter * apothem
+    setInnerTextById('pentagon-area', area)
+    // console.log(area);
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
